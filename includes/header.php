49284ce33dda587afbf4e3925740ebc1ed7b5ab9
Rich Kylle Lauren Dela Cruz <162021520+kyllelauren@users.noreply.github.com>
2024-10-04 11:43:15 +0800

    header

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+
+<header>
+<header>
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <a href="#">Page Header</a>
+    </div>
+
+    <ul class="nav-links">
+      <li><a href="#" class="active">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+
+    <a href="https://mail.google.com" class="gmailButton" target="_blank">Go to Gmail</a>
+
+  </nav>
+</header>
+
+
+</header>
+
